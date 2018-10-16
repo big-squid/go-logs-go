7c82bba6c7b808a33867924c68f53fe24ebb021b
Alexander Webb <awebb@bigsquid.com>
2018-10-16 12:40:29 -0600

    Gofmt

--- logger_test.go
+++ logger_test.go
@@ -1,48 +1,48 @@
 package logging
 
 import (
-  "bufio"
-  "strings"
-  "bytes"
-  "testing"
-  "log"
+	"bufio"
+	"bytes"
+	"log"
+	"strings"
+	"testing"
 )
 
 const logEnv = "LOG_CONFIG"
 
 func TestNew(test *testing.T) {
-  // Make sure the constructor works just fine.
-  logger := New("main", DEBUG, nil)
+	// Make sure the constructor works just fine.
+	logger := New("main", DEBUG, nil)
 
-  // Just run everything to make sure no errors occur.
-  logger.Info("info")
-  logger.Debug("debug")
-  logger.Warn("warn")
-  logger.Error("error")
+	// Just run everything to make sure no errors occur.
+	logger.Info("info")
+	logger.Debug("debug")
+	logger.Warn("warn")
+	logger.Error("error")
 }
 
 // This will test that the root config is honored.
 func TestConfigA(test *testing.T) {
-  var logger Logger
-  logger = New("main", DEBUG, nil)
-  logger.JsonConfig([]byte(`
+	var logger Logger
+	logger = New("main", DEBUG, nil)
+	logger.JsonConfig([]byte(`
     { "level": "INFO"
     }
   `))
-  if logger.Level() != INFO {
-    test.Error("Expected log level to be INFO for `main`")
-  }
+	if logger.Level() != INFO {
+		test.Error("Expected log level to be INFO for `main`")
+	}
 
-  logger = logger.New("main.test")
-  if logger.Level() != INFO {
-    test.Error("Expected log level to be INFO for `main.test`")
-  }
+	logger = logger.New("main.test")
+	if logger.Level() != INFO {
+		test.Error("Expected log level to be INFO for `main.test`")
+	}
 }
 
 func TestConfigB(test *testing.T) {
-  var logger Logger
-  logger = New("main", DEBUG, nil)
-  logger.JsonConfig([]byte(`
+	var logger Logger
+	logger = New("main", DEBUG, nil)
+	logger.JsonConfig([]byte(`
     { "level": "ERROR",
       "loggers": {
         "main": {
@@ -57,35 +57,35 @@ func TestConfigB(test *testing.T) {
     }
   `))
 
-  if logger.Level() != INFO {
-    test.Error("Expected log level to be INFO for `main`")
-  }
+	if logger.Level() != INFO {
+		test.Error("Expected log level to be INFO for `main`")
+	}
 
-  logger = logger.New("main.test")
-  if logger.Level() != FATAL {
-    test.Error("Expected log level to be FATAL for `main.test`")
-  }
+	logger = logger.New("main.test")
+	if logger.Level() != FATAL {
+		test.Error("Expected log level to be FATAL for `main.test`")
+	}
 }
 
 func TestLogLevel(test *testing.T) {
-  logger := New("main", INFO, nil)
+	logger := New("main", INFO, nil)
 
-  var buffer bytes.Buffer
-  writer := bufio.NewWriter(&buffer)
-  log.SetOutput(writer)
-  // Redirect output to this stream.
-  logger.Info("hello")
-  writer.Flush()
+	var buffer bytes.Buffer
+	writer := bufio.NewWriter(&buffer)
+	log.SetOutput(writer)
+	// Redirect output to this stream.
+	logger.Info("hello")
+	writer.Flush()
 
-  if !strings.Contains(buffer.String(), "hello") {
-    test.Errorf("Expected log message `%s` to contain `hello`", buffer.String())
-  }
+	if !strings.Contains(buffer.String(), "hello") {
+		test.Errorf("Expected log message `%s` to contain `hello`", buffer.String())
+	}
 
-  buffer.Reset()
-  logger.Debug("debug msg")
-  writer.Flush()
+	buffer.Reset()
+	logger.Debug("debug msg")
+	writer.Flush()
 
-  if strings.Contains(buffer.String(), "debug msg") {
-    test.Errorf("Expected log message `%s` to omit `debug msg` but the message still got through.", buffer.String())
-  }
+	if strings.Contains(buffer.String(), "debug msg") {
+		test.Errorf("Expected log message `%s` to omit `debug msg` but the message still got through.", buffer.String())
+	}
 }
